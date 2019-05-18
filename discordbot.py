ca309a7867f029326e5a5911886c66036af52720
RPGY100 <yaspad@icloud.com>
2019-05-18 09:29:54 +0900

    first commit

---
+++ discordbot.py
@@ -0,0 +1,51 @@
+# インストールした discord.py を読み込む
+import discord
+
+TOKEN = 3yDT3mrUJYhGopVZuPeMp7VguEVIzAGN
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+@client.event
+async def on_message(message):
+    # メンバーのリストを取得して表示
+    if message.content == '/members':
+        print(message.guild.members)
+    # 役職のリストを取得して表示
+    if message.content == '/roles':
+        print(message.guild.roles)
+    # テキストチャンネルのリストを取得して表示
+    if message.content == '/text_channels':
+        print(message.guild.text_channels)
+    # ボイスチャンネルのリストを取得して表示
+    if message.content == '/voice_channels':
+        print(message.guild.voice_channels)
+    # カテゴリチャンネルのリストを取得して表示
+    if message.content == '/category_channels':
+        print(message.guild.categories)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('/join'):
+        role = discord.utils.get(message.guild.roles, name='member')
+        await message.author.add_roles(role)
+        reply = f'{message.author.mention} ようこそ！'
+        await message.channel.send(reply)
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
